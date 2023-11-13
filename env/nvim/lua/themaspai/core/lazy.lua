078c795cd82a29bcfd5199ae989f387bf5f0f40c
Paiman <paiman.bandi@gmail.com>
2023-11-13 14:37:40 +0700

    add flash

--- env/nvim/lua/themaspai/core/lazy.lua
+++ env/nvim/lua/themaspai/core/lazy.lua
@@ -223,24 +223,18 @@ require('lazy').setup({
     'kevinhwang91/promise-async'
   },
   {
-    "luukvbaal/statuscol.nvim", config = function()
-      -- local builtin = require("statuscol.builtin")
-      require("statuscol").setup({
-        -- configuration goes here, for example:
-        -- relculright = true,
-        -- segments = {
-        --   { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-        --   {
-        --     sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
-        --     click = "v:lua.ScSa"
-        --   },
-        --   { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
-        --   {
-        --     sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-        --     click = "v:lua.ScSa"
-        --   },
-        -- }
-      })
-    end,
+    "luukvbaal/statuscol.nvim"
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+      opts = {},
+      keys = {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   }
 }, {})
---
+++ env/nvim/lua/themaspai/plugins/flash.lua
@@ -0,0 +1,8 @@
+local flash = require('flash')
+
+flash.jump({
+  remote_op = {
+    restore = true,
+    motion = true
+  }
+})
